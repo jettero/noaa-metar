0cff06aa724e36fd063819f89deb61854c630a2d
Paul Miller <jettero@gmail.com>
2009-09-22 07:22:29 -0400

    got this to compile, doubt it works

--- contrib/gweather-cmd/gweather.c
+++ contrib/gweather-cmd/gweather.c
@@ -1,26 +1,34 @@
 
-#include <stdio.h>
-#include <glib-2.0/glib/gtypes> // XXX: this is fairly *NOT PORTABLE*
-#include <weather.h>
+#define GWEATHER_I_KNOW_THIS_IS_UNSTABLE
 
-#ifndef GWEATHER_I_KNOW_THIS_IS_UNSTABLE
+#include <stdio.h>
+#include <libgweather/weather.h>
 
 void got_weather_cb(WeatherInfo *info, gpointer data) {
 }
 
 int main() {
     WeatherLocation *location;
-    WeatherPrefs *prefs;
+    WeatherPrefs prefs;
     gpointer data;
 
-    const gchar trans_name   = "Kalamazoo"; // <_name>
-    const gchar code         = "KAZO"; // <code>
-    const gchar zone         = "MIZ072"; // <zone>
-    const gchar radar        = "mkg"; // <radar>
-    const gchar coordinates  = "42-13-45N 085-32-47W"; // <coordinates>
-    const gchar country_code = "US"; // <iso-code> from <country>
-    const gchar tz_hint      = "America/New_York"; // <tz-hint> from <state>
+    const gchar * trans_name   = "Kalamazoo"; // <_name>
+    const gchar * code         = "KAZO"; // <code>
+    const gchar * zone         = "MIZ072"; // <zone>
+    const gchar * radar        = "mkg"; // <radar>
+    const gchar * coordinates  = "42-13-45N 085-32-47W"; // <coordinates>
+    const gchar * country_code = "US"; // <iso-code> from <country>
+    const gchar * tz_hint      = "America/New_York"; // <tz-hint> from <state>
 
+    // TODO: this needs to be freed I bet, dunno for sure
     location = weather_location_new(trans_name, code, zone, radar, coordinates, country_code, tz_hint);
-    weather_info_new(location, prefs, got_weather_cb, data);
+
+    prefs.type             = FORECAST_ZONE; // ZONE or STATE. ??
+    prefs.radar            = 1;
+    prefs.temperature_unit = TEMP_UNIT_FAHRENHEIT;
+    prefs.speed_unit       = SPEED_UNIT_MPH;
+    prefs.pressure_unit    = PRESSURE_UNIT_ATM;
+    prefs.distance_unit    = DISTANCE_UNIT_MILES;
+
+    weather_info_new(location, &prefs, got_weather_cb, data);
 }
