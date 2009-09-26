ac7ceb6643c4c0969ff1fa30951a7634195d5642
Paul Miller <jettero@gmail.com>
2009-09-26 07:21:57 -0400

    added a few things

---
+++ app/metar/add_metar.js
@@ -0,0 +1,21 @@
+function Add_metarAssistant(args) {
+	Mojo.Log.info("Add_metarAssistant()");
+
+    this.dbo = args[0];
+}
+
+Create_aAssistant.prototype.setup = function() {
+	Mojo.Log.info("Add_metarAssistant()::setup()");
+}
+
+Create_aAssistant.prototype.activate = function(event) {
+}
+
+Create_aAssistant.prototype.deactivate = function(event) {
+}
+
+Create_aAssistant.prototype.cleanup = function(event) {
+    // XXX: What needs to be cleaned up?  Seriously.  Does any of this clean
+    // itself up? or do you have to go through and destroy each object and
+    // click handler?
+}
---
+++ app/metar/metar.js
@@ -0,0 +1,17 @@
+function extract_metar(airport, html) {
+    html = html.replace(/<[^>]+>/g, "");
+
+    var metar = "?";
+
+    var lines = html.split("\n");
+    for(var i=0; i<lines.length; i++) {
+        if( lines[i].substr(0, airport.length) == airport ) {
+            if( lines[i].substr(airport.length).match(/\s[A-Z0-9\/:/-]+$/) ) {
+                metar = lines[i];
+                break;
+            }
+        }
+    }
+
+    return metar;
+}
