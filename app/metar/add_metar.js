df7ac5ab5de788dc0c84e0cc2b01cce2e77b3033
Paul Miller <jettero@gmail.com>
2009-09-26 08:37:18 -0400

    OK, gotta work on j3 project now; this is kindof working though

--- app/metar/add_metar.js
+++ app/metar/add_metar.js
@@ -20,15 +20,28 @@ Add_metarAssistant.prototype.setup = function() {
         this.locations_model.items.push({ 'location': states[i] });
 
     this.controller.setupWidget('gw_locations', attrs, this.locations_model);
+	Mojo.Event.listen(this.controller.get("gw_locations"), Mojo.Event.listTap, this.listClickHandler.bind(this));
 }
 
-Create_aAssistant.prototype.activate = function(event) {
+Add_metarAssistant.prototype.listClickHandler = function(event) {
+    Mojo.Log.info("clicked!!");
+    /*
+    if (event.originalEvent.target.hasClassName('title')) {
+        Mojo.Log.info("Clicked on " + event.item.data);
+
+        var index = event.model.items.indexOf(event.item);
+        Mojo.Log.info("Index is " + index);
+    }        
+    */
 }
 
-Create_aAssistant.prototype.deactivate = function(event) {
+Add_metarAssistant.prototype.activate = function(event) {
 }
 
-Create_aAssistant.prototype.cleanup = function(event) {
+Add_metarAssistant.prototype.deactivate = function(event) {
+}
+
+Add_metarAssistant.prototype.cleanup = function(event) {
     // XXX: What needs to be cleaned up?  Seriously.  Does any of this clean
     // itself up? or do you have to go through and destroy each object and
     // click handler?
