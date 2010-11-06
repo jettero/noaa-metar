0132eb73815e273c80600a5e507b5964a15fba2b
Paul Miller <jettero@gmail.com>
2010-11-06 14:44:28 -0400

    this should work fairly well

---
+++ metar/get_cycles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+d=$(date +%Y-%m-%d)
+mkdir -p $d
+
+set -e
+
+for i in {0..23}; do
+    if [ $i -lt 10 ]; then
+        z=0${i}Z
+    else
+        z=${i}Z
+    fi
+
+    if [ ! -f $d/$z.metar ]; then
+        wget -c -T15 -t100 -O $z ftp://tgftp.nws.noaa.gov/data/observations/metar/cycles/$z.TXT
+        grep '^[A-Z]' $i | sort -u | bzip2 > $d/$z.metar
+        rm $i
+    fi
+done
