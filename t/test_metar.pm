88bc3c52edfcb27d4ea63c6a7a6971e98eee1194
Paul Miller <jettero@gmail.com>
2010-11-07 12:46:17 -0500

    strip html

--- t/test_metar.pm
+++ t/test_metar.pm
@@ -12,6 +12,8 @@ sub process_metar {
     my @lines = capturex(node=>$js, "@_");
     chomp @lines;
 
+    s/<[^>]+>//g for @lines;
+
     tie my %H, 'Tie::IxHash' or die $!;
 
     $H{$_->[0]} = decode(utf8=>$_->[1]) for map {[ m/^([^:]+):\s+(.+)/ ]} @lines;
