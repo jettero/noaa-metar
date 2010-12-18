c78b78138dc05f9786b88030d6ee75f5bcc4405e
Paul Miller <jettero@gmail.com>
2010-12-18 08:18:32 -0500

    show/hide help/about info depending on the mode

--- app/assistants/About.js
+++ app/assistants/About.js
@@ -6,6 +6,11 @@ AboutAssistant.prototype.setup = function() {
     this.controller.get("build-date").update(
         "This instance was built on " + this.buildDate.toLocaleString() + "."
     );
+
+    if( OPT.liteMode ) {
+        $$(".hide-when-lite").each(function(e){e.setStyle({display: 'none'});});
+        $$(".show-when-lite").each(function(e){e.setStyle({display: 'block'});});
+    }
 };
 
 Mojo.Log.info("About()");
--- app/assistants/Help.js
+++ app/assistants/Help.js
@@ -1,4 +1,11 @@
 function HelpAssistant() {
 }
 
+HelpAssistant.prototype.setup = function() {
+    if( OPT.liteMode ) {
+        $$(".hide-when-lite").each(function(e){e.setStyle({display: 'none'});});
+        $$(".show-when-lite").each(function(e){e.setStyle({display: 'block'});});
+    }
+};
+
 Mojo.Log.info("Help()");
